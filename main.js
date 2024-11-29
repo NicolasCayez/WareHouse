e1923d2a3413d1b522e9f1d4b41cf9f05f8a255d
Nicolas Cayez <130738851+NicolasCayez@users.noreply.github.com>
2024-11-29 10:27:08 +0100

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-import { generateHeader } from "./scripts/header";
-import { generateFooter } from "./scripts/footer";
-import { generateDashboard, refreshDashboard } from "./scripts/dashboard";
-
-generateHeader();
-generateFooter();
-
-generateDashboard();
-refreshDashboard();
